6d8159fcd6ee77cb1296e1f342ae3396685ef3b0
Gabe Herbert <gh@gabeherbert.com>
2022-12-12 16:16:47 +0000

    creates template in shortcode

--- dicom-viewer-plugin.php
+++ dicom-viewer-plugin.php
@@ -88,6 +88,14 @@ function dicom_shortcode_func( $atts) {
 	$image_set_title = get_field( 'image_set_title', $post_id );
 	$images = get_field( 'images', $post_id );
 	$image_count = count($images);
+	$one_image_id = (int) $images[$image_count-1]; // @TODO this doesn't work because the images are not sorted
+	$placeholder_image = wp_get_attachment_url( $one_image_id, 'full' );
+
+	// include the template from include/dicom-template.php
+	ob_start();
+	include plugin_dir_path( __FILE__ ) . 'include/dicom-template.php';
+	return ob_get_clean();
+
 }
 
 add_shortcode( 'dicom', 'dicom_shortcode_func' );
