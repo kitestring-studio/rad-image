54082fb648db9a943382c219e6073a18a645d1c2
Gabe Herbert <gh@gabeherbert.com>
2022-12-15 16:27:51 +0000

    commits acf config to php

--- dicom-viewer-plugin.php
+++ dicom-viewer-plugin.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Plugin Name:     DICOM Viewer KS
- * Plugin URI:      PLUGIN SITE HERE
- * Description:     PLUGIN DESCRIPTION HERE
+ * Plugin Name:     DICOM Viewer
+ * Plugin URI:
+ * Description:
  * Author:          Kitestring Studio
  * Author URI:      https://kitestring.studio
  * Text Domain:     dicom-viewer-plugin
@@ -16,7 +16,9 @@
 if ( ! defined( 'ABSPATH' ) ) { exit; }
 
 require_once plugin_dir_path( __FILE__ ) . 'include/dicom-cpt.php';
+require_once plugin_dir_path( __FILE__ ) . 'include/dicom-acf.php';
 require_once plugin_dir_path( __FILE__ ) . 'include/class-dicom-viewer.php';
 
-
-new Dicom_Viewer();
+if( function_exists('acf_add_local_field_group') ){
+	new Dicom_Viewer();
+}
