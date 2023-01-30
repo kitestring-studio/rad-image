608f5333936d75fb7d6f24ff66d384eda59a1a05
Gabe Herbert <gh@gabeherbert.com>
2023-01-30 15:02:32 +0000

    version update 0.1.3; rm old cpt code

--- dicom-viewer-plugin.php
+++ dicom-viewer-plugin.php
@@ -7,7 +7,7 @@
  * Author URI:      https://kitestring.studio
  * Text Domain:     dicom-viewer-plugin
  * Domain Path:     /languages
- * Version:         0.1.2
+ * Version:         0.1.3
  *
  * @package         Dicom_Viewer_Plugin
  */
