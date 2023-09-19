eee0302336c33e3569718fa4749fc9fbfb19f4b7
Gabe Herbert <gh@gabeherbert.com>
2023-09-18 20:58:13 -0700

    move css from template to css file

--- include/keyshot-template.php
+++ include/keyshot-template.php
@@ -1,14 +1,3 @@
 <div id="KeyShotXR-<?php echo $viewer_count; ?>" class="KeyShotXR" style="max-width: 100%;height:0;padding-bottom: <?php echo $this->aspect_ratio * 100; ?>%;" oncontextmenu="return false;">
 <!--	<img style="" id="placeholder" src="--><?php //echo $image_url; ?><!--" alt="--><?php //echo $alt;?><!--">-->
 </div>
-<style>
-	.rad-image__wrapper .KeyShotXR {
-		margin-bottom: 2rem;
-	}
-	.rad-image__wrapper .KeyShotXR img {
-		max-width: 100%;
-	}
-	.KeyShotXR #placeholder:not(:only-child) {
-		display: none;
-	}
-</style>
